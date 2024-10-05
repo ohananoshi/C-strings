5fe7a5439044f3834d4f4d77c68037c5f48159b3
Guilherme Arruda <122743725+ohananoshi@users.noreply.github.com>
2024-10-05 14:47:43 -0300

    Add files via upload
    new_functions: str_vjoin, str_search; readme updated

--- Readme.md
+++ Readme.md
@@ -7,6 +7,7 @@
     - stdint
     - string
     - ctype
+    - stdarg
 
 - #### Datatypes
     - STRING_t
@@ -18,25 +19,33 @@
     
     Attributes:
         str
+        str_len
 
     Functions:
-        count, replace, split, join, from_file, to_file
+        count, search, replace, split, join, vjoin, from_file, to_file
     
     ```
 
 - #### Macros
-    - S(x) ```Abstraction for str_init() function.```
+    ```
+    S(x) - Abstraction for str_init() function.
+
+    END_LIST - Flag for va_args funtions
+        foo(a,b,..., ..., ENDLIST)
+
+    count_t - Macro for datatype of len variables (memory purpose)
+    ```
 
 - #### Functions
 
     ```C
-    uint16_t str_count(const char* src_str, const char* value)
+    count_t str_count(const char* src_str, const char* value)
 
-    Counts the number of times a given element appears in the string.
+    Counts the number of times a given value appears in the string.
 
     Parameters:
         src_str: Source string.
-        value  : value to be counted.
+        value  : Value to be counted.
     
     Return:
         The number of times this value was found.
@@ -44,7 +53,7 @@
     ```
 
     ```C
-    uint8_t str_split(const char* src_str, const char* value, char*** dest_list)
+    count_t str_split(const char* src_str, const char* value, char*** dest_list)
 
     Splits a string using a value as a reference.
 
@@ -54,19 +63,35 @@
         dest_list: Destination char** array (passed by reference).
 
     Return:
-        The number of elements of Destination list.
+        The number of elements of dest_list.
 
     ```
 
     ```C
-    uint8_t str_join(char** dest_str, char* join_str, char** str_list, uint8_t item_count)
+    count_t str_join(char** dest_str, char* join_str, char** str_list, count_t item_count)
 
     Join a list of strings with a 'join string'.
 
     Parameters:
-        dest_str : Destination string (passed by reference).
-        join_str : Used to be appended between the strings.
-        str_list : List of strings to be joined.
+        dest_str  : Destination string (passed by reference).
+        join_str  : Used to be appended between the strings.
+        str_list  : Array of strings to be joined.
+        item_count: Number of items in str_list.
+
+    Return:
+        The new length of dest_str.
+    ```
+
+    ```C
+    count_t str_vjoin(char** dest_str, char* join_str, ...)
+
+    Join a list of strings with a 'join string'.
+
+    Parameters:
+        dest_str  : Destination string (passed by reference).
+        join_str  : Used to be appended between the strings.
+        ...       : Strings to be appended separated by comma.
+                    var_args list must be ended with END_LIST flag.
 
     Return:
         The new length of dest_str.
@@ -82,6 +107,22 @@
         src_str   : Source string.
         old_value : Value to be changed.
         new_value : New value.
+
+    Return:
+        The new length of dest_str.
+    ```
+
+    ```C
+    count_t str_search(const char* src_str, const char* value, count_t** dest_pos, count_t occurrence_num)
+
+    Finds occurrences of the searched value.
+
+    Parameters:
+        src_str       : Source string.
+        value         : Value to be searched.
+        dest_pos      : Destination array to positions of occurrences (passed by reference).
+        occurrence_num: From start to end the limit number of occurrences to be searched.
+                        If occurrence_num == 0, the function will return all occurrences.
 
     Return:
         The new length of dest_str.
--- c_strings.h
+++ c_strings.h
@@ -2,8 +2,8 @@
 
     Author: Guilherme Arruda
 
-    Created in: 28/07/24
-    Last Updated: 29/07/24
+    Created in: 28/jul/24
+    Last Updated: 05/oct/24
 */
 
 //==================================== HEADERS ==============================================
@@ -17,32 +17,36 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 
 //========================================= DATA STRUCTURES ====================================
 
-typedef struct{
+// MACROS
+
+#define S(a) str_init(a)
+#define END_LIST "-->END_LIST<--"
+#define count_t uint16_t
+
+typedef struct STRING_t{
     char *str;
-    uint16_t (*count)(const char* src_str, const char* value);
-    uint8_t (*split)(const char* src_str, const char* value, char*** dest_list);
-    uint8_t (*join)(char** dest_str, char* join_str, char** str_list, uint8_t item_count);
-    uint8_t (*replace)(char** dest_str, const char* src_str, char* old_value, char* new_value);
+    count_t str_len;
+    count_t (*count)(const char* src_str, const char* value);
+    count_t (*split)(const char* src_str, const char* value, char*** dest_list);
+    count_t (*join)(char** dest_str, char* join_str, char** str_list, count_t item_count);
+    count_t (*vjoin)(char** dest_str, char* join_str, ...);
+    count_t (*search)(const char* src_str, const char* value, count_t** dest_pos, count_t ocurrency_num);
+    count_t (*replace)(char** dest_str, const char* src_str, char* old_value, char* new_value);
     size_t (*from_file)(char** dest_str, char* file_path);
     size_t (*to_file)(const char* src_str, char* file_path, char* mode);
 }STRING_t;
 
-// MACROS
+//=============(*vjoin)(*vjoin)(*vjoin)(*vjoin)====================== FUNCTIONS ==============================================
 
-#define S(a) str_init(a)
-
-
-//=================================== FUNCTIONS ==============================================
-
-uint16_t str_count(const char* src_str, const char* value){
-    uint16_t val_len = strlen(value);
-    uint8_t count = 0;
+count_t str_count(const char* src_str, const char* value){
+    count_t val_len = strlen(value), count = 0;
     
-    for(int i = 0; (i + val_len) <= strlen(src_str); i++){
+    for(count_t i = 0; (i + val_len) <= strlen(src_str); i++){
     	if(!strncmp(&src_str[i], value, val_len)){
     		i += val_len - 1;
     		count++;
@@ -52,9 +56,10 @@ uint16_t str_count(const char* src_str, const char* value){
     return count;
 }
 
-uint8_t str_split(const char* src_str, const char* value, char*** dest_list){
-    uint16_t val_len = strlen(value);
-    uint8_t count = 0, last_pos = 0, i;
+count_t str_split(const char* src_str, const char* value, char*** dest_list){
+    count_t val_len = strlen(value);
+    count_t count = 0, last_pos = 0, i;
+    
     (*dest_list) = NULL;
     
     for(i = 0; (i + val_len) <= strlen(src_str); i++){
@@ -78,9 +83,9 @@ uint8_t str_split(const char* src_str, const char* value, char*** dest_list){
     return count;
 }
 
-uint8_t str_join(char** dest_str, char* join_str, char** str_list, uint8_t item_count){
+count_t str_join(char** dest_str, char* join_str, char** str_list, count_t item_count){
     
-    uint8_t count = 0, i;
+    count_t count = 0, i;
     (*dest_str) = (char*)calloc(1, sizeof(char));
 
     for(i = 0; i < item_count - 1; i++){
@@ -97,15 +102,79 @@ uint8_t str_join(char** dest_str, char* join_str, char** str_list, uint8_t item_
     return count;
 }
 
-uint8_t str_replace(char** dest_str, const char* src_str, char* old_value, char* new_value){
-    uint16_t old_val_len = strlen(old_value);
-    uint16_t new_val_len = strlen(new_value);
-    uint16_t dest_len = strlen(src_str);
+count_t str_vjoin(char** dest_str, char* join_str, ...){
+    count_t output_len = 0;
+    (*dest_str) = NULL;
+    
+    va_list args;
+    va_start(args, join_str);
 
-    uint8_t dif = new_val_len - old_val_len;
-    uint8_t last_pos_dest = 0, last_pos_src = 0, i;
+    char* tmp = va_arg(args, char*);
+    
+    while(strcmp(tmp, END_LIST) != 0){
+        output_len += strlen(tmp) + strlen(join_str);
+        (*dest_str) = (char*)realloc((*dest_str), output_len);
+        strcat((*dest_str), tmp);
+        strcat((*dest_str), join_str);
+        tmp = NULL;
+        tmp = va_arg(args, char*);
+    };
 
-    //(*dest_str) = (char*)calloc(strlen(src_str) + n_times*dif, sizeof(char));
+    output_len -= strlen(join_str);
+    (*dest_str) = (char*)realloc((*dest_str), output_len);
+    (*dest_str)[output_len] = '\0';
+
+    va_end(args);
+
+    return output_len;
+}
+
+count_t str_search(const char* src_str, const char* value, count_t** dest_pos, count_t occurrence_num){
+    
+    char* buffer = (char*)calloc(strlen(src_str), sizeof(char));
+    strcpy(buffer, src_str);
+
+    if(*dest_pos != NULL){
+        free(*dest_pos);
+        *dest_pos = NULL;
+    }
+
+    count_t count = 0;
+
+    if(!occurrence_num){
+
+        buffer = strstr(buffer, value);
+        while(buffer != NULL){
+            count++;
+            (*dest_pos) = (count_t*)realloc((*dest_pos), count);
+            (*dest_pos)[count-1] = (count_t)(strlen(src_str) - strlen(buffer));
+            buffer = strstr(&buffer[1], value);
+        }
+
+        return count;
+    }else{
+
+        buffer = strstr(buffer, value);
+        while((buffer != NULL) && (count < occurrence_num)){
+            count++;
+            (*dest_pos) = (count_t*)realloc((*dest_pos), count);
+            (*dest_pos)[count - 1] = (count_t)(strlen(src_str) - strlen(buffer));
+            buffer = strstr(&buffer[1], value);
+        }
+
+        return count;
+    }
+
+};
+
+count_t str_replace(char** dest_str, const char* src_str, char* old_value, char* new_value){
+    count_t old_val_len = strlen(old_value);
+    count_t new_val_len = strlen(new_value);
+    count_t dest_len = strlen(src_str);
+
+    count_t dif = new_val_len - old_val_len;
+    count_t last_pos_dest = 0, last_pos_src = 0, i;
+
     (*dest_str) = (char*)calloc(dest_len, sizeof(char));
 
     for(i = 0; (i + old_val_len) <= strlen(src_str); i++){
@@ -122,7 +191,7 @@ uint8_t str_replace(char** dest_str, const char* src_str, char* old_value, char*
 
     if(last_pos_src < strlen(src_str) - 1) strcat(&(*dest_str)[last_pos_dest], &src_str[last_pos_src]);
 
-    return 1;
+    return dest_len;
 }
 
 size_t str_from_file(char** dest_str, char* file_path){
@@ -158,23 +227,33 @@ size_t str_to_file(const char* src_str, char* file_path, char* mode){
     fwrite(src_str, sizeof(char), strlen(src_str), f);
     fflush(f);
 
+
+    rewind(f);
+    fseek(f, 0, SEEK_END);
+    size_t f_len = ftell(f);
+
     fclose(f);
 
-    return strlen(src_str);
+    return f_len;
 }
 
-STRING_t* str_init(char* str){
+STRING_t* str_init(const char* str){
     STRING_t *s = (STRING_t*)calloc(1, sizeof(STRING_t));
 
-    s->str = str;
+    s->str = (char*)calloc(strlen(str), sizeof(char));
+    strcpy(s->str, str);
+
+    s->str_len = strlen(str);
 
     s->count = str_count;
-    s->join = str_join;
-    s->replace = str_replace;
     s->split = str_split;
+    s->join = str_join;
+    s->vjoin = str_vjoin;
+    s->search = str_search;
+    s->replace = str_replace;
     s->to_file = str_to_file;
     s->from_file = str_from_file;
 }
 
 
-#endif _C_STRINGS_HEADER // END C_strings
+#endif // END C_strings
